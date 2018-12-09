b3a0b3170a46cb7a05f836ef1cb3b0ae4530cfab
DESKTOP-3K8IPPF\LIZHEN <lizhen_init@163.com>
2018-12-09 21:49:42 +0800

    no message

--- src/main/java/com/ccm/permission/system/ShiroProjectApp.java
+++ src/main/java/com/ccm/permission/system/ShiroProjectApp.java
@@ -1,11 +1,11 @@
-package com.ccm;
+package com.ccm.permission.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShiroProjectApp{
+public class ShiroProjectApp {
     public static void main(String[] args) {
-        SpringApplication.run(ShiroProjectApp.class,args);
+        SpringApplication.run(ShiroProjectApp.class, args);
     }
 }
